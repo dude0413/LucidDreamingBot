0fe9c88af21696262477227380108b3e58f7f145
dude0413 <dude0413@gmail.com>
2017-07-09 14:06:02 -0400

    Add files via upload

---
+++ redditLDbot.py
@@ -0,0 +1,45 @@
+import praw
+import config
+import re
+
+
+bot = praw.Reddit(user_agent='LDbot v0.1',
+                  client_id=config.client_id,
+                  client_secret=config.client_secret,
+                  username=config.username,
+                  password=config.password)
+
+reply = "Lucid Dreaming means you are aware that you are dreaming. This can mean with or without control. Use " \
+        "this information to make your decision. This is a bot. Please contact /u/dude0413 for bugs or" \
+        " suggestions. First bot version v0.1"
+
+with open('posts_replied_to', 'r') as f:
+    posts_replied_to = f.read()
+    posts_replied_to = posts_replied_to.split('\n')
+    posts_replied_to = list(filter(None, posts_replied_to))
+
+subreddit = bot.subreddit('Cool_Bot_Testing')
+for submission in subreddit.hot(limit=5):
+    if submission.id not in posts_replied_to:
+        if re.search("was this a lucid dream?", submission.title, re.IGNORECASE):
+            submission.reply(reply)
+            print("Bot replying to : ", submission.title)
+            posts_replied_to.append(submission.id)
+    if submission.id not in posts_replied_to:
+        if re.search("Was this a LD?", submission.title, re.IGNORECASE):
+            submission.reply(reply)
+            print("Bot replying to: ", submission.title)
+            posts_replied_to.append(submission.id)
+    if submission.id not in posts_replied_to:
+        if re.search("Was this a Lucid Dream?", submission.title, re.IGNORECASE):
+            submission.reply(reply)
+            print("Bot replying to: ", submission.title)
+            posts_replied_to.append(submission.id)
+    if submission.id not in posts_replied_to:
+        if re.search("Lucid Dream?", submission.title, re.IGNORECASE):
+            submission.reply(reply)
+            print("Bot replying to: ", submission.title)
+            posts_replied_to.append(submission.id)
+with open("posts_replied_to", "w") as f:
+    for post_id in posts_replied_to:
+        f.write(post_id + "\n")
