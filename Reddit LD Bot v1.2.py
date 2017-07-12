894348df255dad813a7f670e3045c492cc7b8270
dude0413 <dude0413@gmail.com>
2017-07-12 06:46:55 -0400

    Update Reddit LD Bot v1.2.py

--- Reddit LD Bot v1.2.py
+++ Reddit LD Bot v1.2.py
@@ -1,25 +1,32 @@
 import re
 import praw
-from LDbot import config
+import config
 
 bot = praw.Reddit(user_agent='LDbot v1.0',
                   client_id=config.client_id,
                   client_secret=config.client_secret,
                   username=config.username,
                   password=config.password)
+with open('posts_replied_to') as f:
+    for i, l in enumerate(f):
+        number_of_lines = i + 2
+
 reply = "Lucid Dreaming means you are aware that you are dreaming. This can mean with or without control. Use " \
         "this information to make your decision. This is a bot. Please contact /u/dude0413 for bugs or" \
-        " suggestions. First bot version v0.1"
+        " suggestions. First bot version v0.1. Has replied to: " + str(number_of_lines) + ' posts.'
+
 with open('posts_replied_to', 'r') as f:
     posts_replied_to = f.read()
     posts_replied_to = posts_replied_to.split('\n')
     posts_replied_to = list(filter(None, posts_replied_to))
-subreddit = bot.subreddit('Cool_Bot_Testing')
+
+subreddit = bot.subreddit('LucidDreaming')
 for submission in subreddit.new(limit=10):
     id_of_post = submission.id
     title_of_post = submission.title
     author_of_post = submission.author
-    post_names = ['was this a lucid dream?', 'Was this a LD?', 'Was this a Lucid Dream?', 'Lucid Dream?']
+
+    post_names = ['was this a lucid dream?', 'Was this a LD?', 'Was this a Lucid Dream?', 'lucid dream']
     post_names_length = len(post_names) - 1
     x = 0
     while x < post_names_length:
@@ -29,6 +36,7 @@ for submission in subreddit.new(limit=10):
                 submission.reply(reply)
                 print('Bot replying to : "', title_of_post + '"' + ' by: "' + str(author_of_post) + '"')
                 posts_replied_to.append(id_of_post)
+
 with open("posts_replied_to", "w") as f:
     for post_id in posts_replied_to:
         f.write(post_id + "\n")
