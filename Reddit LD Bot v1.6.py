e36e178cf5e5fbcdd0313ba5676b57d3cb58793b
dude0413 <dude0413@gmail.com>
2017-07-20 17:06:49 -0400

    Update Reddit LD Bot v1.6.py

--- Reddit LD Bot v1.6.py
+++ Reddit LD Bot v1.6.py
@@ -1,5 +1,8 @@
 import re, praw, config
 version = 'v1.6'
+# Common subreddits #
+LD = 'LucidDreaming'
+CBT = 'Cool_Bot_Testing'
 # Login into the bot #
 bot = praw.Reddit(user_agent='LDbot v1.0',
                   client_id=config.client_id,
@@ -16,13 +19,13 @@ with open('replies_to_reply', 'r') as c:
     for i, l in enumerate(c):
         number_of_lines_for_post_replies = i + 1
         number_of_segments = number_of_lines_for_post_replies / 2
-# Opens the posts_replied_to txt file and reads the lines
+# Opens the posts_replied_to txt file and reads the lines #
 with open('posts_replied_to', 'r') as f:
     posts_replied_to = f.read()
     posts_replied_to = posts_replied_to.split('\n')
     posts_replied_to = list(filter(None, posts_replied_to))
 # Enters the subreddit #
-subreddit = bot.subreddit('LucidDreaming')
+subreddit = bot.subreddit(CBT)
 # Main work for posts #
 for submission in subreddit.new(limit=50):
         id_of_post = submission.id
@@ -35,7 +38,7 @@ for submission in subreddit.new(limit=50):
                 look_for_list = lines[variable * 2].split(';')
                 new_look_for_list = look_for_list[:-1]
                 replies = lines[variable * 2 + 1]
-                final_reply = str(replies + ' Has replied to: ' + str_number_of_post_replies + ' posts.  This is a bot. Please contact /u/dude0413 for bugs or suggestions.' + version)
+                final_reply = str(replies + '\n\n Has replied to: ' + str_number_of_post_replies + ' posts.  This is a bot. Please contact /u/dude0413 for bugs or suggestions.' + version)
             length_of_look_for_list = len(new_look_for_list)
             variable += 1
             x = 0
