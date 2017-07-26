391dd872ad8c7e6f7fd4be04f5640f1a7cc58605
dude0413 <dude0413@gmail.com>
2017-07-26 08:25:56 -0400

    Update Reddit LD Bot v1.6.py

--- Reddit LD Bot v1.6.py
+++ Reddit LD Bot v1.6.py
@@ -1,6 +1,6 @@
 import re, praw, config
 version = 'v1.6'
-# Common subreddits #
+# Common sub-reddits #
 LD = 'LucidDreaming'
 CBT = 'Cool_Bot_Testing'
 # Login into the bot #
@@ -9,23 +9,21 @@ bot = praw.Reddit(user_agent='LDbot v1.0',
                   client_secret=config.client_secret,
                   username=config.username,
                   password=config.password)
-# Gets the number of posts that the bot has replied to #
+# Gets the number of posts that the bot has replied to and reads and splits the contents of the txt file #
 with open('posts_replied_to') as r:
     for i, l in enumerate(r):
         number_of_post_replies = i + 2
         str_number_of_post_replies = str(number_of_post_replies)
+    posts_replied_to = r.read()
+    posts_replied_to = posts_replied_to.split('\n')
+    posts_replied_to = list(filter(None, posts_replied_to))
 # Get the number of lines of the replies and look for. Also numbers the segments of the post replies #
 with open('replies_to_reply', 'r') as c:
     for i, l in enumerate(c):
         number_of_lines_for_post_replies = i + 1
         number_of_segments = number_of_lines_for_post_replies / 2
-# Opens the posts_replied_to txt file and reads the lines #
-with open('posts_replied_to', 'r') as f:
-    posts_replied_to = f.read()
-    posts_replied_to = posts_replied_to.split('\n')
-    posts_replied_to = list(filter(None, posts_replied_to))
 # Enters the subreddit #
-subreddit = bot.subreddit(CBT)
+subreddit = bot.subreddit(LD)
 # Main work for posts #
 for submission in subreddit.new(limit=50):
         id_of_post = submission.id
@@ -38,7 +36,7 @@ for submission in subreddit.new(limit=50):
                 look_for_list = lines[variable * 2].split(';')
                 new_look_for_list = look_for_list[:-1]
                 replies = lines[variable * 2 + 1]
-                final_reply = str(replies + '\n\n Has replied to: ' + str_number_of_post_replies + ' posts.  This is a bot. Please contact /u/dude0413 for bugs or suggestions.' + version)
+                final_reply = str(replies + '\n\n Has replied to: ' + str_number_of_post_replies + ' posts.  This is a bot. Please contact /u/dude0413 for bugs or suggestions.\n\n' + version)
             length_of_look_for_list = len(new_look_for_list)
             variable += 1
             x = 0
